6efc3f0f60491fe7f230ab87b45820be13b3cc9e
Reham <reham.ahmed@optomatica.com>
2019-07-27 22:05:51 +0200

    add splash screen and MainScreen

---
+++ src/MainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+export class MainScreen extends React.Component {
+    render(){
+       return(
+        <View>
+            <Text>MAIN...</Text>
+        </View>
+       )
+    }
+}
